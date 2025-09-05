75e7eeaf4a66873461c9083ff4c26c2d280614df
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2025-09-05 00:22:37 -0500

    update footer returns quicklink (brief)

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -72,7 +72,7 @@ const Footer = () => {
                     <Button color="inherit" onClick={() => navigate("/support")}>Help Center</Button><br />
                     <Button color="inherit" onClick={() => navigate("/support")}>Contact Us</Button><br />
                     <Button color="inherit" onClick={() => navigate("/shippingreturns")}>Shipping Info</Button><br />
-                    <Button color="inherit">Returns</Button>
+                    <Button color="inherit" onClick={() => navigate("/shippingreturns")}>Returns</Button><br />
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
                     <Typography variant='h6' gutterBottom>Legal</Typography>
