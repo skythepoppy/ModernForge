b47af3fd0362cf417a8e254d6c8ebb243ae26b5c
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2025-09-10 04:33:44 -0500

    troubleshooting index.js ( change /api/users)

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.use('/api/users', userRoutes);
 
 
 // --- MySQL setup ---
