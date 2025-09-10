c74c20e8df691a4e4d58bbb25edcc81db87b963d
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2025-09-10 03:53:39 -0500

    create a centralized axios instance

---
+++ src/customer/components/api/api.js
@@ -0,0 +1,26 @@
+// src/api/api.js
+import axios from "axios";
+
+let token = null;
+
+// Function to set token dynamically
+export const setToken = (newToken) => {
+    token = newToken;
+};
+
+const api = axios.create({
+    baseURL: "http://localhost:5050/api", // adjust your backend URL
+});
+
+// Add Authorization header automatically
+api.interceptors.request.use(
+    (config) => {
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
