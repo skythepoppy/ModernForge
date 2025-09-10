3ece3ca6acff3b94ef26e6c27f6c08c17f8ea499
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2025-09-10 05:33:10 -0500

    shorten JWT token for new user (improved security without relying on http cookies)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -56,7 +56,7 @@ router.post("/register", async (req, res) => {
     const token = jwt.sign(
       { id: newUser.id, role: newUser.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "30m" }
     );
 
     // --- Nodemailer: send welcome email ---
